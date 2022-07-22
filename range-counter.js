5992aac09845348c3fad4a88e40d237b356f8362
pritishg3 <ghate.pritish3@gmail.com>
2022-07-22 11:34:24 +0530

    Resolved eslintrc issues

--- range-counter.js
+++ range-counter.js
@@ -1,42 +1,39 @@
-
-
 function sort(listOfNum) {
-    return listOfNum.sort((a, b) => a - b);    
+  return listOfNum.sort((a, b) => a - b);
 }
 
 function resetCounterMap(counter, rangeList) {
-    counter = 1;
-    rangeList.length = 0;
+  counter = 1;
+  rangeList.length = 0;
 }
 
 function fillRangeCountMap(sortedNum, rangeList, rangeMap) {
-    counter=0;
-    for(let i =0; i < sortedNum.length; i++) {                
-        if(sortedNum[i+1] - sortedNum[i] === 1 ) {
-            rangeList.push(sortedNum[i]);           
-        }
-        else {
-            rangeList.push(sortedNum[i]);
-            counter = rangeList.length;
-            rangeMap[rangeList] = counter;
-            resetCounterMap(counter, rangeList);
-        }
+  let counter = 0;
+  for (let i = 0; i < sortedNum.length; i++) {
+    if (sortedNum[i + 1] - sortedNum[i] === 1) {
+      rangeList.push(sortedNum[i]);
+    } else {
+      rangeList.push(sortedNum[i]);
+      counter = rangeList.length;
+      rangeMap[rangeList] = counter;
+      resetCounterMap(counter, rangeList);
     }
+  }
 }
 
 function printRangeValue(listOfNum) {
-    let sortedNum = sort(listOfNum);    
-    let rangeMap = {};
-    let rangeList = []   
-    fillRangeCountMap(sortedNum, rangeList, rangeMap);
-    console.log(rangeMap);
+  const sortedNum = sort(listOfNum);
+  const rangeMap = {};
+  const rangeList = [];
+  fillRangeCountMap(sortedNum, rangeList, rangeMap);
+  console.log(rangeMap);
 }
 
-printRangeValue([2,1,10,11,12,13,3]);
+printRangeValue([2, 1, 10, 11, 12, 13, 3]);
 
 module.exports = {
-    sort,
-    printRangeValue,
-    resetCounterMap,
-    fillRangeCountMap
-}
+  sort,
+  printRangeValue,
+  resetCounterMap,
+  fillRangeCountMap,
+};
